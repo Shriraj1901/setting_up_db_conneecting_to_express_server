edc61ef8c372928f29f9cb2b2ce42ca05a461639
shreeraj <shreerajjadhav19@gmail.com>
2025-01-31 13:13:20 +0530

    code upadeted

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const { resolve } = require('path');
 
+require("dotenv").config();
+const express = require("express");
+const { resolve } = require("path");
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.URI;
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Database."))
+  .catch((err) => console.log(`Error: ${err}`));
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
 });
 
 app.listen(port, () => {
